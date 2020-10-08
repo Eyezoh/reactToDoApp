94dfb022cdd670a774ffd1784ef746b5645b48bf
Eyezoh <amumpaireisaac@gmail.com>
2020-10-08 14:17:04 +0300

    Updates

---
+++ precache-manifest.fb27c5442fa7eb5014e0fcd36fd2acbc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cf0c16271434f82adfa1c28871acf92",
+    "url": "/reactToDoApp/index.html"
+  },
+  {
+    "revision": "1c52b3b55cd48ef6a1b1",
+    "url": "/reactToDoApp/static/css/main.20fb01b6.chunk.css"
+  },
+  {
+    "revision": "6a84c7da698c8cf8a3b7",
+    "url": "/reactToDoApp/static/js/2.4510165c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactToDoApp/static/js/2.4510165c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c52b3b55cd48ef6a1b1",
+    "url": "/reactToDoApp/static/js/main.680d9546.chunk.js"
+  },
+  {
+    "revision": "3311bf5e0962f4d2cc75",
+    "url": "/reactToDoApp/static/js/runtime-main.4cb0bbb1.js"
+  }
+]);
