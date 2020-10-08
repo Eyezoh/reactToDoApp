5515fb51aa3bc5a3d4e34fc3c0cb9e7ec9addce2
Eyezoh <amumpaireisaac@gmail.com>
2020-10-08 14:13:56 +0300

    deploying to gh pages

---
+++ src/Navigation.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navigation = ()=>(
+<div className="navigation">
+   <h1 className="logo">My To-Do list</h1>
+   <div className="navigation_menu"></div> 
+   <p className="navigation_menuItem">Notes</p>
+   <p className="navigation_menuItem">About</p>
+
+</div>
+)
+export default Navigation
